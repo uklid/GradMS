8d71316f3829fcacccdf72c5315c4e661a9968b2
Uklid <uklid.y@gmail.com>
2017-03-29 04:30:36 +0700

    metronics added

--- GradMS/App_Start/BundleConfig.cs
+++ GradMS/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace GradMS
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/signin.css"));
         }
     }
 }
